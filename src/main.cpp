2ac99e4fc0f401e84cdef4120871647f1ac8eb92
Julien C <julien.jc.cote@gmail.com>
2018-02-14 15:45:46 -0500

    Refactored for cleanliness and easier future developments

---
+++ src/main.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <chrono>
+#include <thread>
+#include <future>
+#include <mutex>
+
+#include <boost/multiprecision/cpp_dec_float.hpp>
+
+#include "simple_complex.h"
+#include "mandelbrot.h"
+
+//typedef boost::multiprecision::cpp_dec_float_50 mp_number;
+typedef long double mp_number;
+
+int kWindowsSizeX = 1920;
+int kWindowsSizeY = 1080;
+
+int kResolution = 800;
+int kNbThread = 8;
+
+/*mp_number k_xmin = -2;
+mp_number k_xmax = 0.5;
+mp_number k_ymin = -1.25;
+mp_number k_ymax = 1.25;*/
+
+//mp_number center_point_x = -1.985540371654130485531439267191269851811165434636382820704394766801377; //0.25467168048098116
+//mp_number center_point_y = 0.000000000000000000000000000001565120217211466101983496092509512479178; //0.00054712897737948770
+
+
+int main( int argc, char** argv ) {
+	Mandelbrot<long double> mandelbrot{};
+	sf::RenderWindow window(sf::VideoMode(kWindowsSizeX, kWindowsSizeY), "fractal_display", sf::Style::Close);
+	window.setVerticalSyncEnabled(true);
+
+	sf::Image image;
+	sf::Texture texture;
+	sf::Sprite sprite;
+
+	image.create(kWindowsSizeX, kWindowsSizeY, sf::Color::White);
+
+	bool display_changed = true;
+
+	// std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+	// mandelbrot_set(k_xmin, k_xmax, k_ymin, k_ymax, image);
+	// std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+
+	// auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+	// std::cout << duration << '\n';
+
+	//image.saveToFile("C:\\Users\\Julien\\Pictures\\mandel.png");
+
+	//int frame = 0;
+	while (window.isOpen()/*&& frame < 3000*/)
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (event.type == sf::Event::KeyPressed) {
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+					mandelbrot.move_left(0.05);
+					display_changed = true;
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+					mandelbrot.move_right(0.05);
+					display_changed = true;
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+					mandelbrot.move_up(0.05);
+					display_changed = true;
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+					mandelbrot.move_down(0.05);
+					display_changed = true;
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Add)) {
+					mandelbrot.move_in(1.1);
+					display_changed = true;
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Subtract)) {
+					mandelbrot.move_out(1.1);
+					display_changed = true;
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad1)) {
+					mandelbrot.precision_increase(2);
+					display_changed = true;
+				}
+				else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad4)) {
+					mandelbrot.precision_decrease(2);
+					display_changed = true;
+				}
+			}
+		}
+
+		/*VIDEO RECORD HACK */
+		/*++frame;
+		zoom_factor *= 1.01;
+		std::string path = "C:\\Users\\Julien\\Pictures\\fractal_video\\mandel" + std::to_string(frame) + ".png";
+		image.saveToFile(path);
+		mp_number delta_x = std::abs((k_initial_xmax - k_initial_xmin) / zoom_factor);
+		mp_number delta_y = std::abs((k_initial_ymax - k_initial_ymin) / zoom_factor);
+		//kDepth += (kDepth / 0.01 == 0 ? 1 : kDepth * 0.01);
+		kDepth *= 1.01;
+		k_xmin = center_point_x - (2 / zoom_factor);
+		k_xmax = center_point_x + (2 / zoom_factor);
+		k_ymin = center_point_y - (2 / zoom_factor);
+		k_ymax = center_point_y + (2 / zoom_factor);
+		display_changed = true;
+
+		/*END OF VIDEO HACK*/
+
+		if (display_changed) {
+			image.create(kWindowsSizeX, kWindowsSizeY, sf::Color::White);
+			mandelbrot.render(image);
+		}
+
+		window.clear();
+		texture.loadFromImage(image);
+		sprite.setTexture(texture, true);
+		window.draw(sprite);
+		window.display();
+		display_changed = false;
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(20));
+	}
+	return 0;
+}
---
+++ src/mandelbrot.h
@@ -0,0 +1,127 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "simple_complex.h"
+
+#include <iostream>
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <chrono>
+#include <thread>
+#include <future>
+#include <mutex>
+
+template<class T>
+class Mandelbrot{
+    public:
+        Mandelbrot(){};
+
+        int mandelbrot(SimpleComplex& value) {
+            SimpleComplex base = value;
+            for (int i = 0; i < kDepth; ++i) {
+                if (4 < value.magnitude_squared()) {
+                    return i;
+                }
+                value *= value;
+                value += base;
+            }
+            return kDepth;
+        }
+
+        void partial_brot(const T& xmin, const  T& xmax, T&& ymin, T&& ymax, int&& window_x_min, const int& window_x_max, int&& window_y_min, int&& window_y_max, sf::Image& image) {
+            std::map<std::pair<int, int>, int> out;
+
+            for (int x = window_x_min; x < window_x_max; ++x) {
+                for (int y = window_y_min; y < window_y_max; ++y) {
+                    T delta_x = (x * 1.0 / (window_x_max - window_x_min)) * (xmax - xmin);
+                    T delta_y = (y * 1.0 / (window_y_max - window_y_min)) * (ymax - ymin);
+                    SimpleComplex coordinate(xmin + delta_x, k_ymin + delta_y);
+                    int deviation = mandelbrot(coordinate);
+                    if (deviation < kDepth) {
+                        image.setPixel(x, y, sf::Color((deviation * 7) % 256, (deviation * 41) % 256, (deviation * 127) % 256));
+                    }
+                }
+            }
+        }
+
+
+        void mandelbrot_set(const T& xmin, const T& xmax, const T& ymin, const T& ymax, sf::Image& image) {
+            std::vector<std::thread> parts;
+
+            T delta_y = ymax - ymin;
+
+            for (int i = 0; i < kNbThread; ++i) {
+                T a = ymin + (i * 1.0 / kNbThread * delta_y);
+                T b = ymin + ((i + 1.0) / kNbThread * delta_y);
+
+                int c = static_cast<int>(std::round(kWindowsSizeY * i * 1.0 / kNbThread));
+                int d = static_cast<int>(std::round(kWindowsSizeY * (i + 1.0) / kNbThread));
+
+                parts.push_back(std::thread(&Mandelbrot::partial_brot, this, std::ref(xmin), std::ref(xmax), std::move(a), std::move(b), 0, std::ref(kWindowsSizeX), std::move(c), std::move(d), std::ref(image)));
+            }
+
+            for (auto &t : parts) {
+                t.join();
+            }
+        }
+        void render(sf::Image& image) {
+            k_xmin = center_point_x - (2 / zoom_factor);
+			k_xmax = center_point_x + (2 / zoom_factor);
+			k_ymin = center_point_y - (2 / zoom_factor);
+			k_ymax = center_point_y + (2 / zoom_factor);
+            mandelbrot_set(k_xmin, k_xmax, k_ymin, k_ymax, image);
+        }
+
+        void move_left(float factor){
+            T delta = (k_xmax - k_xmin) * factor;
+			center_point_x -= delta;
+        };
+
+        void move_right(float factor){
+            T delta = (k_xmax - k_xmin) * factor;
+            center_point_x += delta;
+        };
+        void move_up(float factor){
+            T delta = (k_ymax - k_ymin) * factor;
+            center_point_y -= delta;
+        };
+        void move_down(float factor){
+            T delta = (k_ymax - k_ymin) * factor;
+            center_point_y += delta;
+        };
+        void move_in(float factor){
+            zoom_factor *= factor;
+        };
+        void move_out(float factor){
+            zoom_factor /= factor;
+        };
+        void precision_increase(float factor){
+            kDepth *= factor;
+        };
+        void precision_decrease(float factor){
+            kDepth /= factor;
+        };
+
+        T center_point_x = 0.25467168048098116;
+        T center_point_y = 0.00054712897737948770;
+
+        T k_initial_xmin = center_point_x - 2; //0.0013299 //1.3299
+        T k_initial_xmax = center_point_x + 2;
+        T k_initial_ymin = center_point_y - 2; //0.980002
+        T k_initial_ymax = center_point_y + 2;
+
+        T k_xmin = center_point_x - 2; //0.0013299 //1.3299
+        T k_xmax = center_point_x + 2;
+        T k_ymin = center_point_y - 2; //0.980002
+        T k_ymax = center_point_y + 2;
+
+        int kDepth = 100;
+
+        int kWindowsSizeX = 1920;
+        int kWindowsSizeY = 1080;
+
+        int kResolution = 800;
+        int kNbThread = 8;
+        double zoom_factor = 1;
+};
+
